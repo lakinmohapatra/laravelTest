d15ea6899cda25f2058772c1e4a6cd010b859faa
Debabrata Patra <debabratapatra12@gmail.com>
2016-03-04 11:08:35 +0530

    Changed layout.

--- app/Test.php
+++ app/Test.php
@@ -7,16 +7,16 @@ use filemaker_laravel\Database\Eloquent\Model;
 
 class Test extends Model
 {
-    protected $layoutName = 'Web_GCT';
+    protected $layoutName = 'Contracts_Test';
     
     public function fm()
     {
         //return Test::test();
         //return Test::testQuery();
         //return Test::testElo();
-        $test = $this->where('EmailFlag', '==', '1', 'or')
-                     ->where('GiftCardNumber', '==', '*')
-                     ->orderBy('GiftCard_AmountLeft', 'asc')
+        $test = $this->where('_kf_ContractSerNum', '==', '1', 'or')
+                     ->where('_kf_ContractSerNum', '==', '*')
+                     ->orderBy('ContractName', 'asc')
                      ->skip(10)
                      ->take(5)
                      ->get();
